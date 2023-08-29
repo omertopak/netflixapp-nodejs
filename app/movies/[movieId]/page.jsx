c80ebe493ad8acefcbcf18da18cecdee8e8ca622
omer <omertopak1@gmail.com>
2023-08-29 20:50:17 +0100

    first commit

---
+++ app/movies/[movieId]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const movieId = ({params}) => {
+  console.log(params);
+  return (
+    <div>movie detail</div>
+  )
+}
+
+export default movieId
---
+++ app/movies/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+    title:"movies"
+  return (
+    <div>movies</div>
+  )
+}
+
+export default page
